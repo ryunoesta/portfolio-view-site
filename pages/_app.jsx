7e9e286c18785bfa2d04e3c8c477703ecfecfb1e
Ryunosuke Iwashita <ryuno0907soccerboy@gmail.com>
2023-02-22 21:28:02 +0900

    Change contents explain

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Learn how to use the thirdweb Auth SDK to create an NFT Gated Website"
+          content="テックアイエス広島校卒業生のポートフォリオ一覧。特定のNFTの所有者のみ閲覧可能なサイト。"
         />
       </Head>
       <Component {...pageProps} />
